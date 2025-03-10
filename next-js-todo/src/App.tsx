1318d625a4bf2f557382fc70da0d7993e4986b96
123456466 <sunji7378@gmail.com>
2025-03-11 01:39:43 +0900

    Feat : todo 업데이트

--- next-js-todo/src/App.tsx
+++ next-js-todo/src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { getTodos, type Todo } from "./todos";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoItme, setTodoItme] = useState<Todo[]>([]);
+  useEffect(() => {
+    getTodos().then((data) => setTodoItme(data));
+  }, []);
+
+  const [title, setTitle] = useState("");
+
+  const upadtaTodoHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const addTodoHandle = async () => {
+    if (title === "") {
+      return alert("내용을 입력해주세요");
+    }
+
+    const newTodo: Todo = {
+      id: crypto.randomUUID(),
+      title,
+      completed: false,
+    };
+
+    await fetch("http://localhost:4000/todos", {
+      method: "POST",
+      body: JSON.stringify(newTodo),
+    });
+    setTitle("");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TodoList todoList={todoItme} />
+      <input
+        type="text"
+        placeholder="할일을 적어주세요."
+        onChange={upadtaTodoHandle}
+        value={title}
+      />
+      <button onClick={addTodoHandle}>등록</button>
     </>
-  )
+  );
 }
 
-export default App
+type TodoListProps = { todoList: Todo[] };
+function TodoList({ todoList }: TodoListProps) {
+  return (
+    <>
+      {todoList.map((todo) => (
+        <TodoItem key={todo.id} {...todo} />
+      ))}
+    </>
+  );
+}
+
+function TodoItem({ id, title, completed }: Todo) {
+  return (
+    <div>
+      <div>{id}</div>
+      <div>{title}</div>
+      <div>{`${completed}`}</div>
+    </div>
+  );
+}
+
+export default App;
