152b5bbc80c92d46a4870711b9c8a5884cab8596
123456466 <sunji7378@gmail.com>
2025-03-13 17:54:41 +0900

    feat : 리스트 가져오기

---
+++ basic-todo/app/page.tsx
@@ -0,0 +1,11 @@
+import TodoList from "@/components/todo/TodoList";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <section>
+      <TodoList />
+    </section>
+  );
+};
+export default HomePage;
