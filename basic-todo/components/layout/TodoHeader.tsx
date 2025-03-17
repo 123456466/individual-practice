f923d6c2e3e1c579ea6d2499456cc2da14cae74a
123456466 <sunji7378@gmail.com>
2025-03-17 16:39:24 +0900

    feat : 헤더 컴포넌트 생성

---
+++ basic-todo/components/layout/TodoHeader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoHeader = () => {
+  return (
+    <header className="mb-5">
+      <div className="container mx-auto p-6">
+        <h1 className="text-4xl font-bold">TodoHeader</h1>
+      </div>
+    </header>
+  );
+};
+
+export default TodoHeader;
